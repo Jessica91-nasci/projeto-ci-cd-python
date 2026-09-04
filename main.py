519271408740a46a35a8547e72e2bc9c9633dd6e
Jessica91-nasci <jemari.nasci@gmail.com>
2026-09-04 17:21:36 -0300

    adiciona algoritmo básico de criptografia

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Iniciando o sistema de Criptografia Simples")
+
+print("Iniciando o sistema de Criptografia Simples")
+
+def criptografar(texto, deslocamento):
+    resultado = ""
+    for caractere in texto:
+        if caractere.isalpha():
+            codigo = ord(caractere) + deslocamento
+            resultado += chr(codigo)
+        else:
+            resultado += caractere
+    return resultado
